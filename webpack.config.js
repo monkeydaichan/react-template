9a3ecda075d0a879cd79e947f4ea41c86dd45873
Daichi Yasuda <monkeydaichi124@gmail.com>
2019-07-27 16:16:10 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+  entry: {
+    main: path.resolve(__dirname, './src/', 'index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    publicPath: '/',
+    filename: '[name]-[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: [/node_modules/],
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { url: false, modules: true } }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  serve: {
+    open: true,
+    port: 8080,
+    content: path.resolve(__dirname, 'public')
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
+}
