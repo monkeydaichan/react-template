e4905e690079fa533f216636224d7cdadea2f71b
Daichi Yasuda <monkeydaichi124@gmail.com>
2019-07-30 14:57:05 +0900

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(jpg|png)$/,
+        loaders: 'url-loader'
+      },
+      {
         test: /\.jsx?$/,
         exclude: [/node_modules/],
         loader: 'babel-loader'
